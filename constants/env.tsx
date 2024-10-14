797905d7be1a6bdcbda76643ddb64f9de89dd651
ssajudn <sajudinmaruf@gmail.com>
2024-10-14 23:59:46 +0700

    feat: setup supabase client

---
+++ constants/env.tsx
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
+export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+export const DATABASE_URL = process.env.EXPO_PUBLIC_DATABASE_URL;
+export const DIRECT_URL = process.env.EXPO_PUBLIC_DIRECT_URL;
